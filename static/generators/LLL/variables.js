caca84c41d2ba60366977816b4ae4ec126003d74
mode80 <mode80>
2014-03-18 14:14:46 -0600

    variables.js converted

--- static/generators/LLL/variables.js
+++ static/generators/LLL/variables.js
@@ -17,18 +17,18 @@ goog.provide('Blockly.LLL.variables');
 goog.require('Blockly.LLL');
 
 
-Blockly.JavaScript['variables_get'] = function(block) {
+Blockly.LLL['variables_get'] = function(block) {
   // Variable getter.
   var code = Blockly.LLL.variableDB_.getName(block.getFieldValue('VAR'),
       Blockly.Variables.NAME_TYPE);
-  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+  return [code, Blockly.LLL.ORDER_ATOMIC];
 };
 
-Blockly.JavaScript['variables_set'] = function(block) {
+Blockly.LLL['variables_set'] = function(block) {
   // Variable setter.
-  var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
-      Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
-  var varName = Blockly.JavaScript.variableDB_.getName(
+  var argument0 = Blockly.LLL.valueToCode(block, 'VALUE',
+      Blockly.LLL.ORDER_ASSIGNMENT) || '0';
+  var varName = Blockly.LLL.variableDB_.getName(
       block.getFieldValue('VAR'), Blockly.Variables.NAME_TYPE);
   return '("' + varName + '"' + argument0 + ')\n';
 };
